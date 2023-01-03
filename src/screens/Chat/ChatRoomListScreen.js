70c1ab495ba9e7aa50bc1a1e9035f48463a6d595
hubbbbbbbbb <hubyu9234@gmail.com>
2023-01-03 11:09:42 -0800

    Update ChatRoomListScreen.js
    Added stylesheet

--- src/screens/Chat/ChatRoomListScreen.js
+++ src/screens/Chat/ChatRoomListScreen.js
@@ -1,9 +1,17 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function ChatRoomListScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View style={styles.container}>
       <Text>ChatRoomList Screen</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
